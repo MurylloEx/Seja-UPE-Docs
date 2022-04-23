afc456999ab76e9ed83cdc49337ce7bcd9e265ba
MurylloEx <muryllopimenta@gmail.com>
2022-04-23 01:13:13 -0300

    :memo: docs: Added RF/RNF codes in front of requirements names.

--- template/seja-upe-theme.template.js
+++ template/seja-upe-theme.template.js
@@ -11,6 +11,9 @@ module.exports = (options) => {
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="${options.stylesheet}">
     <style>
+      * {
+        text: justify!important;
+      }
       table {
         width: 100%!important;
         display: table!important;
